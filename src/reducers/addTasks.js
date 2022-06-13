5d66c250959a6fbad0f33c15033e6d1ac919f90f
Karpov Yuri <yurick@bk.ru>
2022-06-13 16:10:03 +0300

    fix: Удалить console.log из addTasks

--- src/reducers/addTasks.js
+++ src/reducers/addTasks.js
@@ -1,7 +1,6 @@
 import { ADD_TASK, initialState } from '../utils/constants';
 
 const tasks = (state = initialState, { type, id, text, username, email, status }) => {
-  console.log(type, id, text, username, email, status);
   switch (type) {
     case ADD_TASK:
       return [
