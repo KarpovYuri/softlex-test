027cad383d069b048dc0cf6dfbe8bec69888b69d
Karpov Yuri <yurick@bk.ru>
2022-06-14 11:54:36 +0300

    fix: Исправить редактирование отметки о выполнении

--- src/components/task-list-item/task-list-item.jsx
+++ src/components/task-list-item/task-list-item.jsx
@@ -35,18 +35,23 @@ const TaskListItem = ({ id, text, username, email, status, onEditTask }) => {
     switch (status) {
       case 0:
         setIsStatus(10);
+        status = 10;
         break;
       case 1:
         setIsStatus(11);
+        status = 11;
         break;
       case 10:
         setIsStatus(0);
+        status = 0;
         break;
       case 11:
         setIsStatus(1);
+        status = 1;
         break;
     }
-    onEditTask({ id, text: isText, status: isStatus });
+
+    onEditTask({ id, text: isText, status });
   }
 
 
@@ -70,7 +75,7 @@ const TaskListItem = ({ id, text, username, email, status, onEditTask }) => {
       <span className='text-hidden col-6 col-sm-5 col-md-3 mt-2 mt-sm-0'>{email}</span>
       <button
         disabled={isLogdIn ? '' : 'disabled'}
-        onClick={() => { handleEditStatus(isStatus) }}
+        onClick={() => { handleEditStatus(status) }}
         className={`btn-status offset-9 col-3 offset-sm-0 col-sm-2 col-md-1 mt-3 mt-sm-0 px-2 py-1 bg-opacity-10 border
       ${isStatus === 10 || isStatus === 11 ? 'text-success bg-success border-success' : 'text-warning bg-warning border-warning'}`}>
         {isStatus === 10 || isStatus === 11 ? 'Done' : 'Active'}
